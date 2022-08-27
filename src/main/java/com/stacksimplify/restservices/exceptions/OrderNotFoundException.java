e873f56346fd5b7b59e64cbea7f7515310da4f83
GwangKwang <gwangkwang@naver.com>
2022-08-27 16:50:41 +0900

    First Commit - OneToMany

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/OrderNotFoundException.java
@@ -0,0 +1,10 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class OrderNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 8483848611770842416L;
+
+	public OrderNotFoundException(String message) {
+		super(message);
+	}
+}
